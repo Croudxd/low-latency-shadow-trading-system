f953eb2c5947a4e54560e3e951c8bab04cd9d23c
croudxd <benjamincroud@gmail.com>
2026-02-09 02:59:15 +0000

    Added candle class just holds OHLCV.

---
+++ include/candle.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+class Candle
+{
+    public:
+        Candle() = default;
+        Candle(long _open, long _high, long _low, long _close, long _volume) : open(_open), high(_high), low(_low), close(_close), volume(_volume)
+        {
+        }
+        void print()
+        {
+            std::cout << "[CANDLE]:" << std::endl
+                << "OPEN: " << open<< std::endl
+                << "HIGH: " << high<< std::endl
+                << "LOW: " << low << std::endl
+                << "CLOSE: " << close<< std::endl
+                << "VOLUME: " << volume << std::endl;
+        }
+
+    private:
+        long open;
+        long high;
+        long low;
+        long close;
+        long volume;
+
+};
