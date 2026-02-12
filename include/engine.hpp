907c4ab7c1a5ee85b466dee7effcec00660ac40a
croudxd <benjamincroud@gmail.com>
2026-02-12 20:55:12 +0000

    Moved main to a separate engine file, tried to make the code more abstract/reuseable with some templates which i think works well. COuld probably be improved a little i think its slightly messy.

---
+++ include/engine.hpp
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <atomic>
+#include <candle.hpp>
+#include <cstdint>
+#include <fcntl.h>
+#include <order.hpp>
+#include <order_book.hpp>
+#include <sys/mman.h>
+#include <thread>
+#include <unistd.h>
+
+namespace engine
+{
+    namespace global
+    {
+        static constexpr int BUCKET_SIZE     = 1 * 100000;
+        static constexpr int BUFFER_CAPACITY = 16384;
+    } // namespace global
+
+    namespace mem
+    {
+        struct Data
+        {
+            uint64_t id;
+            uint64_t size;
+            int32_t  price;
+            int8_t   side;
+            int8_t   action;
+            int8_t   status;
+            uint8_t  pad1[1];
+        };
+
+        template <typename T> struct memory_layout
+        {
+            volatile uint64_t write_idx;
+            uint8_t           pad1[56];
+            volatile uint64_t read_idx;
+            uint8_t           pad2[56];
+            T                 buffer[global::BUFFER_CAPACITY];
+        };
+
+        enum class Mem_flags
+        {
+            CONSUMER,
+            PRODUCER,
+        };
+    } // namespace mem
+
+    class Engine
+    {
+
+    public:
+        Engine() = default;
+
+        template <typename T> T* mem_map(const char* path, mem::Mem_flags flag)
+        {
+            int fd = 0;
+            if (flag == mem::Mem_flags::CONSUMER)
+            {
+                fd = open(path, O_RDWR);
+                while (fd == -1)
+                {
+                    sleep(1);
+                    fd = open(path, O_RDWR);
+                }
+            }
+            if (flag == mem::Mem_flags::PRODUCER)
+            {
+                fd = open(path, O_RDWR | O_CREAT, 0666);
+                ftruncate(fd, sizeof(T));
+            }
+
+            void* ptr = mmap(NULL, sizeof(T), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+            if (ptr == MAP_FAILED)
+            {
+                perror("mmap failed");
+                return nullptr;
+            }
+            std::cout << "Connected to memory" << std::endl;
+            return static_cast<T*>(ptr);
+        }
+
+        void send_candle(mem::memory_layout<Candle>* candle_shm, Candle candle)
+        {
+            uint64_t local_write_idx = candle_shm->write_idx;
+            uint64_t cached_read_idx = candle_shm->read_idx;
+            if (local_write_idx - cached_read_idx >= global::BUFFER_CAPACITY)
+            {
+                cached_read_idx = candle_shm->read_idx;
+            }
+            if (local_write_idx - cached_read_idx >= global::BUFFER_CAPACITY)
+            {
+                return;
+            }
+
+            candle_shm->buffer[local_write_idx % global::BUFFER_CAPACITY] = candle;
+            std::atomic_thread_fence(std::memory_order_release);
+            candle_shm->write_idx = local_write_idx + 1;
+        }
+
+        void connect()
+        {
+            rust_order     = mem_map<mem::memory_layout<mem::Data>>("/dev/shm/hft_ring", mem::Mem_flags::CONSUMER);
+            candle_mem     = mem_map<mem::memory_layout<Candle>>("/dev/shm/hft_candle", mem::Mem_flags::PRODUCER);
+            strategy_order = mem_map<mem::memory_layout<mem::Data>>("/dev/shm/hft_order", mem::Mem_flags::CONSUMER);
+
+            rust_local_read_idx     = rust_order->write_idx;
+            strategy_local_read_idx = strategy_order->write_idx;
+        }
+
+        void strategy_order_func(Order_book& book)
+        {
+            uint64_t current_write_idx = strategy_order->write_idx;
+
+            if (strategy_local_read_idx < current_write_idx)
+            {
+                int       slot = strategy_local_read_idx % global::BUFFER_CAPACITY;
+                mem::Data raw  = strategy_order->buffer[slot];
+                auto      side = (raw.side == 0) ? Order_type::buy : Order_type::sell;
+                Order     ord  = { side, raw.price, raw.size, raw.id };
+
+                if (raw.action == 2)
+                {
+                    book.cancel_order(raw.id);
+                }
+                else
+                {
+                    book.add_order(ord, Flags::MATCH);
+                }
+
+                strategy_local_read_idx++;
+            }
+            strategy_order->read_idx = strategy_local_read_idx;
+        }
+
+        void rust_function(Order_book& book)
+        {
+
+            uint64_t current_write_idx = rust_order->write_idx;
+            if (rust_local_read_idx < current_write_idx)
+            {
+                int       slot = rust_local_read_idx % global::BUFFER_CAPACITY;
+                mem::Data raw  = rust_order->buffer[slot];
+                auto      side = (raw.side == 0) ? Order_type::buy : Order_type::sell;
+                Order     ord  = { side, raw.price, raw.size, raw.id };
+                /** status = 1 is a trade not an order so we dont add to the book.*/
+                if (raw.status == 1)
+                {
+                    std::cout << "." << std::flush;
+                    if (current_open == 0)
+                        current_open = raw.price;
+                    if (raw.price > current_high)
+                        current_high = raw.price;
+                    if (raw.price < current_low)
+                        current_low = raw.price;
+
+                    current_local_bucket_size += raw.size;
+
+                    if (current_local_bucket_size >= global::BUCKET_SIZE)
+                    {
+                        long   close = raw.price;
+                        Candle candle
+                            = Candle { current_open, current_high, current_low, close, current_local_bucket_size };
+                        candle.print();
+                        send_candle(candle_mem, candle);
+                        current_local_bucket_size = 0;
+                        current_open              = 0;
+                        current_high              = 0;
+                        current_low               = std::numeric_limits<long>::max();
+                    }
+                }
+                else if (raw.action == 2)
+                {
+                    book.cancel_order(raw.id);
+                }
+                else
+                {
+                    book.add_order(ord, Flags::NONMATCH);
+                }
+                rust_local_read_idx++;
+            }
+            rust_order->read_idx = rust_local_read_idx;
+        }
+
+        void run()
+        {
+            Order_book book = Order_book {};
+            while (true)
+            {
+                strategy_order_func(book);
+                rust_function(book);
+            }
+        }
+
+    private:
+        mem::memory_layout<mem::Data>* rust_order;
+        mem::memory_layout<mem::Data>* strategy_order;
+        mem::memory_layout<Candle>*    candle_mem;
+
+        uint64_t rust_local_read_idx     = 0;
+        uint64_t strategy_local_read_idx = 0;
+
+        long current_local_bucket_size = 0;
+        long current_open              = 0;
+        long current_high              = 0;
+        long current_low               = std::numeric_limits<long>::max();
+    };
+} // namespace engine
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/** 
+/**
  * Okay we have two parts to an order book.
  * The bids which is sorted from highest to lowest (highest at the top)
  * and the asks which is sorted lowest to highest.
@@ -12,169 +12,16 @@
  * A user might want to cancel an order, so we need to implement a way to find and cancel it.
  *
  * we might have a market order, basically buy x amount of shares without caring for price.
- * So we pick the top 
+ * So we pick the top
  *
  * an order needs. An ID, price, size and a side (buying or selling)
  * */
 
-#include "order.hpp"
-#include "candle.hpp"
-#include "order_book.hpp"
+#include "engine.hpp"
 
-#include <cstdint>
-#include <iostream>
-#include <fcntl.h>
-#include <limits>
-#include <sys/mman.h>
-#include <unistd.h>
-#include <thread>
-
-/** multiple by 1 million because rust feeder does this to avoid floats. So just normalizing*/
-static constexpr int BUCKET_SIZE = 1 * 100000; 
-static constexpr int BUFFER_CAPACITY = 16384; 
-
-struct Data 
+int main()
 {
-    uint64_t   id ; 
-    uint64_t   size ; 
-    int32_t    price ; 
-    int8_t     side ;  
-    int8_t     action ;
-    int8_t     status ;
-    uint8_t    pad1[1];
-};
-
-struct Shared_memory_layout 
-{
-    volatile uint64_t write_idx; 
-    uint8_t           pad1[56];
-    volatile uint64_t read_idx; 
-    uint8_t           pad2[56];
-    Data              buffer[BUFFER_CAPACITY];
-};
-
-struct Candle_shared_memory_layout 
-{
-    volatile uint64_t write_idx; 
-    uint8_t           pad1[56];
-    volatile uint64_t read_idx; 
-    uint8_t           pad2[56];
-    Candle            buffer[BUFFER_CAPACITY];
-};
-
-
-void send_candle(Candle_shared_memory_layout* candle_shm, Candle candle)
-{
-    uint64_t local_write_idx = candle_shm->write_idx;
-    uint64_t cached_read_idx = candle_shm->read_idx; 
-        if (local_write_idx - cached_read_idx >= BUFFER_CAPACITY) 
-        {
-            cached_read_idx = candle_shm->read_idx;
-            
-        }
-        if (local_write_idx - cached_read_idx >= BUFFER_CAPACITY) 
-        {
-            return;
-        }
- 
-        candle_shm->buffer[local_write_idx % BUFFER_CAPACITY] = candle;
-        std::atomic_thread_fence(std::memory_order_release);
-        candle_shm->write_idx = local_write_idx + 1;
+    engine::Engine eng = engine::Engine();
+    eng.connect();
+    eng.run();
 }
-
-int main() 
-{
-    Order_book book = Order_book{};
-    int fd = open("/dev/shm/hft_ring", O_RDWR);
-    while (fd == -1) 
-    {
-        std::cout << "Waiting for Rust feeder...\n";
-        sleep(1);
-        fd = open("/dev/shm/hft_ring", O_RDWR);
-    }
-    void* ptr = mmap(NULL, sizeof(Shared_memory_layout), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (ptr == MAP_FAILED) 
-    {
-        perror("mmap failed"); 
-        return 1;
-    }
-    Shared_memory_layout* shm = static_cast<Shared_memory_layout*>(ptr);
-    uint64_t local_read_idx = shm->write_idx;
-    shm->read_idx = local_read_idx;
-    std::cout << "Connected! Watching memory...\n";
-
-
-    int candle_fd = open("/dev/shm/hft_candle", O_RDWR | O_CREAT, 0666); 
-    ftruncate(candle_fd, sizeof(Candle_shared_memory_layout)); 
-    void* candle_ptr = mmap(NULL, sizeof(Candle_shared_memory_layout), PROT_READ | PROT_WRITE, MAP_SHARED, candle_fd, 0);
-    if (candle_ptr == MAP_FAILED) { perror("mmap failed"); return 1; }
-    Candle_shared_memory_layout* candle_shm = static_cast<Candle_shared_memory_layout*>(candle_ptr);
-
-    size_t count = 0;
-    std::cout << "Producer starting..." << std::endl;
-
-
-
-    long local_bucket_size= 0;
-    long open = 0;
-    long high = 0;
-    long low = std::numeric_limits<long>::max();
-    long close = 0;
-
-    while (true)
-    {
-        uint64_t current_write_idx = shm->write_idx;
-        if (local_read_idx < current_write_idx) 
-        {
-            int slot = local_read_idx % BUFFER_CAPACITY;
-            Data raw = shm->buffer[slot];
-            auto side = (raw.side == 0) ? Order_type::buy : Order_type::sell;
-            Order ord = {side, raw.price, raw.size, raw.id};
-            /** status = 1 is a trade not an order so we dont add to the book.*/
-            if (raw.status == 1)
-            {
-                std::cout << "." << std::flush; 
-                if (open == 0)
-                {
-                    open = raw.price;
-                }
-                if (raw.price > high)
-                {
-                    high = raw.price;
-                } 
-                if (raw.price < low)
-                {
-                    low = raw.price;
-                } 
-                local_bucket_size += raw.size;
-                if (local_bucket_size >= BUCKET_SIZE)
-                {
-                    close = raw.price; 
-                    Candle candle = Candle{open, high, low, close, local_bucket_size};
-                    candle.print();
-                    send_candle(candle_shm, candle);
-                    local_bucket_size = 0;
-                    open = 0;
-                    high = 0;
-                    low = std::numeric_limits<long>::max();
-                }
-            }
-            else if (raw.action == 2)
-            {
-                book.cancel_order(raw.id);
-            }
-            else 
-            {
-                book.add_order(ord, Flags::NONMATCH);
-            }
-            local_read_idx++;
-            shm->read_idx = local_read_idx; 
-        }
-        else 
-        {
-            std::this_thread::yield();
-        }
-    }
-}
-
-
